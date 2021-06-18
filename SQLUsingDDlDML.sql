b1db3db2542ed96220b3650ddf0c7146d77004df
MaheshSampath123 <83757151+MaheshSampath123@users.noreply.github.com>
2021-06-18 09:57:28 +0530

    Clauses

---
+++ SQLUsingDDlDML.sql
@@ -0,0 +1,7 @@
+create table sam(num int not null,name varchar(50),salary float)
+insert into sam values(1,'sampath',25000)
+select * from sam
+insert into sam values(2,'',56000)
+insert into sam values(3,'',0)
+insert into sam values(0,'',0)
+insert into sam values(NULL,'NULL',NULL)
